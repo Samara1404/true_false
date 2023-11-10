e485a23ab022a43ee1b00cc6ee66764794ae1ac8
Samara1404 <stambekova1404@gmail.com>
2023-11-10 21:54:47 +0600

    true_false

---
+++ bin/true_false.dart
@@ -0,0 +1,19 @@
+main() {  
+   bool check;   
+   int val1=12; 
+   int val2=9; 
+    
+  
+   check=(val1>val2); 
+   print(check);    
+}
+
+void main() {
+  bool isRaining = true;
+
+  if (isRaining) {
+    print("Bring an umbrella.");
+  } else {
+    print("Enjoy the weather!");
+  }
+}
---
+++ test/true_false_test.dart
@@ -0,0 +1,8 @@
+import 'package:true_false/true_false.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
